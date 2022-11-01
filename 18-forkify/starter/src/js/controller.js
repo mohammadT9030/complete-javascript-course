47fdfeb1b6a5d8448b6e9bd984375d0db5ab10a9
mohammadT9030 <torabi9030@gmail.com>
2022-11-01 18:59:15 +0330

    import es5 modules and icons and render spinner

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,3 +1,7 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import icons from 'url:../img/icons.svg'
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,29 +15,44 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+function renderSpinner(parentEl) {
+  console.log(parentEl)
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+  parentEl.innerHtml = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
 async function showRecipe() {
   try {
+    // 1) get data (loading recipe)
+    renderSpinner(recipeContainer);
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
     const json = await res.json();
 
-    if(!res.ok) throw new Error(`${json.message} (${res.status})`)
+    if (!res.ok) throw new Error(`${json.message} (${res.status})`);
 
     const recipe = {
-      cookingTime : json.data.recipe.cooking_time,
+      cookingTime: json.data.recipe.cooking_time,
       id: json.data.recipe.id,
-      image : json.data.recipe.image_url,
+      image: json.data.recipe.image_url,
       ingredients: json.data.recipe.ingredients,
       publisher: json.data.recipe.publisher,
       servings: json.data.recipe.servings,
       source: json.data.recipe.source_url,
       title: json.data.recipe.title,
-    }
+    };
 
-    console.log(json)
-    console.log(recipe)
+    // 2) render data
+    const markup = `
 
+    `;
   } catch (err) {
     alert(err);
   }
