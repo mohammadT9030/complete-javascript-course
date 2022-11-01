5177b5fe4beb6ce787f540a3b73d6302bcaa17a7
mohammadT9030 <torabi9030@gmail.com>
2022-11-01 16:19:20 +0330

    fetch a recipe and refactore it

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+async function showRecipe() {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const json = await res.json();
+
+    if(!res.ok) throw new Error(`${json.message} (${res.status})`)
+
+    const recipe = {
+      cookingTime : json.data.recipe.cooking_time,
+      id: json.data.recipe.id,
+      image : json.data.recipe.image_url,
+      ingredients: json.data.recipe.ingredients,
+      publisher: json.data.recipe.publisher,
+      servings: json.data.recipe.servings,
+      source: json.data.recipe.source_url,
+      title: json.data.recipe.title,
+    }
+
+    console.log(json)
+    console.log(recipe)
+
+  } catch (err) {
+    alert(err);
+  }
+}
+showRecipe();
